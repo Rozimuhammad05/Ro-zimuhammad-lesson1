eb058a8c6e4a1c88f6ebcf6748b570b722836767
Rozimuhammad05 <roziabdusamadov2005.10@gmail.com>
2024-01-13 11:00:24 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var userName = prompt("Ismingizni kiriting?");
+var userSurname = prompt("Familiyangizni kiriting?");
+var userAge = prompt("Yoshingizni kiriting?");
+var userEmail = prompt("E-mailingizni kiriting?");
+var userMoney = prompt("Ayirboshlamoqchi bo'lgan pulingizni  kiriting?");
+var DOLLAR_UZS=12400;
+console.log("ismi:",  userName, "\n", "familiyasi:",  userSurname, "\n",  "yoshi:", userAge, "\n", "emaili:",  userEmail, "\n", "so'mi:", userMoney*DOLLAR_UZS);
+
